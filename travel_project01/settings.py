67027fa5b605d3f276ed33855f6a7bf30e9e4d3c
HashinMuhammed <hashpv226@gmail.com>
2023-08-21 17:09:23 +0530

    commit

--- travel_project01/settings.py
+++ travel_project01/settings.py
@@ -77,12 +77,11 @@ WSGI_APPLICATION = 'travel_project01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'travel_projects02',
-        'ADMIN':'root',
-        'PASSWORD':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
@@ -119,13 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_URL=[
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT= BASE_DIR / 'media/'
+
 
 
 
